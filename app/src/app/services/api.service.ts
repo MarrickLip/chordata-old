324c2a8624e893f6ac5e9f9a1fda9224dacc574e
Marrick Lip <m@rrick.dev>
2020-11-29 11:35:52 +1300

    fix responsive layout of projects page

--- app/src/app/services/api.service.ts
+++ app/src/app/services/api.service.ts
@@ -19,6 +19,7 @@ export interface ProjectSummary {
     name: string,
     description: string,
     image: string,
+    labels: string[],
   }
 }
 
@@ -29,6 +30,7 @@ const projects: ProjectSummary[] = [
       name: 'Maungahuka / Auckland Islands (2020)',
       description: 'The Auckland Islands are an archipelago of New Zealand, lying 465 kilometres (290 mi) south of the South Island. The main Auckland Island occupies 510 km2 (200 sq mi)',
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Auckland_Islands_-_STS089-743-5.jpg/500px-Auckland_Islands_-_STS089-743-5.jpg',
+      labels: ['Trail Cameras', 'Eradication'],
     }
   },
   {
@@ -37,6 +39,7 @@ const projects: ProjectSummary[] = [
       name: 'Eastern Forrest',
       description: 'Footage from the Eastern Forest trials',
       image: 'https://www.doc.govt.nz/globalassets/images/places/nelson-tasman/heaphy-track/heaphy-forest-1200.jpg',
+      labels: ['Trail Cameras'],
     }
   }
 ];
