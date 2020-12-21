fc7849113315afc834f305b3541d8e77baa80c05
Marrick Lip <m@rrick.dev>
2020-12-21 05:22:35 +0000

    slight upload modal tweaks

---
+++ app/src/app/pages/project-details/upload-modal/step-device/step-device.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StepDeviceComponent } from './step-device.component';
+
+describe('StepDeviceComponent', () => {
+  let component: StepDeviceComponent;
+  let fixture: ComponentFixture<StepDeviceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StepDeviceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StepDeviceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/src/app/pages/project-details/upload-modal/step-device/step-device.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-device',
+  templateUrl: './step-device.component.html',
+  styleUrls: ['./step-device.component.css']
+})
+export class StepDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
