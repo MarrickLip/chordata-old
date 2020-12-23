c32e282d4b93a46081c0abbdcec1f482eb6b86e3
Marrick Lip <m@rrick.dev>
2020-12-23 03:28:10 +0000

    add more boilerplate API Gateway / Lambda

---
+++ engine/api/deployments/handlers.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const handle: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: '[Chordata API] Deployments',
+      path: event.path,
+      method: event.httpMethod,
+    }, null, 2),
+  };
+}
