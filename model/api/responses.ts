55f95185361a2b97568db04b076646aa0e2be450
Marrick Lip <m@rrick.dev>
2020-12-23 01:34:37 +0000

    use serverless typescript template to begin building engine

---
+++ model/api/responses.ts
@@ -0,0 +1,21 @@
+export type GetProjectResponse = {
+    id: string,
+    type: string,
+    info: {
+        name: string,
+        description: string,
+        image: string,
+        tags: string[]
+    }
+};
+
+export type ListProjectsResponse = Array<{
+    id: string,
+    type: string,
+    info: {
+        name: string,
+        description: string,
+        image: string,
+        tags: string[]
+    }
+}>;
