fc4788b47043d9b05c1018ebd129f7d2c9778554
Marrick Lip <m@rrick.dev>
2020-12-27 22:28:56 +0000

    add dummy step-metadata component

---
+++ app/src/app/pages/project-details/upload-modal/step-metadata/step-metadata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-metadata',
+  templateUrl: './step-metadata.component.html',
+  styleUrls: ['./step-metadata.component.css']
+})
+export class StepMetadataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
