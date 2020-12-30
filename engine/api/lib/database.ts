fb5b38bd146c7d70ce59938347fc4229a4f82e3b
Marrick Lip <m@rrick.dev>
2020-12-30 05:20:34 +0000

    styling tweaks to metadataform (brand colours + multiline notes field)

--- engine/api/lib/database.ts
+++ engine/api/lib/database.ts
@@ -1,8 +1,11 @@
-import { MongoClient, Logger } from 'mongodb'
-Logger.setLevel('debug');
+import { MongoClient } from 'mongodb'
 
 const uri = `mongodb+srv://admin:${process.env.DB_PASSWORD}@chordata.v9yqd.mongodb.net/chordata?retryWrites=true&w=majority`
-const client = new MongoClient(uri, { useNewUrlParser: true })
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    loggerLevel: 'debug',
+})
 
 let connection = undefined
 
